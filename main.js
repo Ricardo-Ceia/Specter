840fae96cec32210b4e5d176124a92f28a7892cc
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-12-25 00:49:32 +0000

    add SSE endpoint for real-time browser streaming

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
+const http = require('http');
 const fs = require('fs');
 const readline = require('readline');
 
-const FIFO_PATH = `/tmp/terminal_time_tracker_${process.env.USER}/stream`;
+const FIFO_PATH = `/tmp/terminal_time_
+tracker_${process.env.USER}/stream`;
 
-const stream = fs.createReadStream(FIFO_PATH,{encoding: 'utf8'});
-const rl = readline.createInterface({input: stream});
+// Store connected SSE clients
+const clients = new Set();
 
-rl.on('line', ((line)=>{
-  const data = JSON.parse(line);
-  console.log(data);
-}));
+function startFifoReader() {
+  const stream = fs.createReadStream(FIFO_PATH, {encoding: 'utf8' });
+  const rl = readline.createInterface({ input: stream});
 
-rl.on('close',  ()=>{
-  console.log('Stream closed');
-});
+  rl.on('line', (line) => {
+    for (const res of clients) {
+      res.write(`data: ${line}\n\n`);
+    }
+  });
+
+  rl.on('close', () => {
+    setTimeout(startFifoReader, 1000);
+  });
+}
+
+const server = http.createServer((req,
+  res) => {
+    if (req.url === '/events') {
+      res.writeHead(200, {
+        'Content-Type':
+        'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+      });
+
+      clients.add(res);
+      req.on('close', () =>
+        clients.delete(res));
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html' });
+      res.end('<html>...</html>');
+    }
+  });
+
+server.listen(3000);
+startFifoReader();
